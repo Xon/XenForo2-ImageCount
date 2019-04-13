d8579e8489c267fb5cdf4caec77586ff97c63a3f
Xon <tazipper1@hotmail.com>
2019-04-13 14:20:02 +0800

    Do not error when getMessagePreparer is called on a post without a thread or forum

--- upload/src/addons/SV/ImageCount/XF/Service/Post/Preparer.php
+++ upload/src/addons/SV/ImageCount/XF/Service/Post/Preparer.php
@@ -9,12 +9,20 @@ class Preparer extends XFCP_Preparer
         $messagePreparer = parent::getMessagePreparer($format);
 
         $post = $this->getPost();
-        /** @var \SV\ImageCount\XF\Entity\User $user */
-        $user = \XF::visitor();
-        $maxValue = $user->getForumMessageMaxImages($post->Thread->Forum);
-        if ($maxValue !== false)
+        $thread = $post->Thread;
+        if ($thread)
         {
-            $messagePreparer->setConstraint('maxImages', $maxValue);
+            $forum = $thread->Forum;
+            if ($forum)
+            {
+                /** @var \SV\ImageCount\XF\Entity\User $user */
+                $user = \XF::visitor();
+                $maxValue = $user->getForumMessageMaxImages($forum);
+                if ($maxValue !== false)
+                {
+                    $messagePreparer->setConstraint('maxImages', $maxValue);
+                }
+            }
         }
 
         return $messagePreparer;
